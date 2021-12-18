00da87b3aeb83a0cd473879725b86ec778683b18
FavariCristian <cristian.favari.studenti@isii.it>
2021-12-18 01:38:01 +0100

    implementata la funzione di controllo delle risposte e di salvataggio dei questionari .

---
+++ risultato.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+include('template_header.php');
+include('dal_questionario.php');
+
+if (isset($_POST["username"])) {
+    $_SESSION["username"] = $_POST["username"];
+    registra_utente();
+    header('Location:il_mio_profilo.php');
+}
+$id_questionario = $_POST['id_questionario'];
+$domande = trova_domande($id_questionario);
+?>
+
+<?php
+function segna_risposte($domande)
+{
+    $lista_risposte = [];
+    foreach($domande as $d)
+        array_push($lista_risposte, $_POST[$d['id_domanda']]);
+    //var_dump($lista_risposte);
+    return ($lista_risposte);
+}
+function confronta_risposte($lista_risposte, $id_questionario)
+{
+    $punteggio = 0;
+    $lista_risposte_corrette = trova_risposte_corrette($id_questionario);
+    //var_dump($lista_risposte_corrette);
+    for ($i = 0; $i < count($lista_risposte); $i++)
+        if ($lista_risposte[$i] == $lista_risposte_corrette[$i]['id_risposta'])
+            $punteggio++;
+    return $punteggio;
+}
+
+$lista_risposte = segna_risposte($domande);
+$punteggio = confronta_risposte($lista_risposte, $id_questionario);
+registra_questionario($punteggio, $_SESSION["id_utente"], $id_questionario);
+
+//var_dump($punteggio);
+//var_dump($_SESSION["id_utente"]);
+//var_dump($id_questionario);
+?>
+
+<?php
+include('template_footer.php');
+?>
